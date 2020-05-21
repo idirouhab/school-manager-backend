2d846284acb202c1f2af906f50e515b56527bb58
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-05-21 10:04:26 +0200

    remove icon

--- app/events/email.emmiter.js
+++ app/events/email.emmiter.js
@@ -1,13 +1,13 @@
 const events = require('events');
 const eventEmitter = new events.EventEmitter();
-const userProvider = require("../providers/user.provider")
+const userProvider = require("../providers/user.provider");
 const nodemailer = require('nodemailer');
 
 eventEmitter.on('notify_teacher_new_exam', (examId, answer) => {
     userProvider.findUserByExamId(examId).then(data => {
         const text = `Hola ${data.userId.name}, ${answer.playerName} ha completado la terea "${data.text}" con una nota de ${answer.score} sobre ${data.questions.length}.`
         const message = {
-            from: `Exam Manager 👥<${process.env.MESSAGE_FROM}>`,
+            from: `Exam Manager <${process.env.MESSAGE_FROM}>`,
             to: data.userId.username,
             subject: `Nuevo examen realizado`,
             text: text

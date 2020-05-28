4450d21aaeed5f3fd7a699f0f26401387824092f
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-05-28 14:16:29 +0200

    Use tinaptic as email

--- app/events/email.emmiter.js
+++ app/events/email.emmiter.js
@@ -7,7 +7,7 @@ eventEmitter.on('notify_teacher_new_exam', (examId, answer) => {
     userProvider.findUserByExamId(examId).then(data => {
         const text = `Hola ${data.userId.name}, ${answer.playerName} ha completado la terea "${data.text}" con una nota de ${answer.score} sobre ${data.questions.length}.`
         const message = {
-            from: `Exam Manager <${process.env.MESSAGE_FROM}>`,
+            from: `Tinaptic <${process.env.MESSAGE_FROM}>`,
             to: data.userId.username,
             subject: `Nuevo examen realizado`,
             text: text,

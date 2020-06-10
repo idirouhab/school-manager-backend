189b87c4d2192b634ed241ae75feacff451026a3
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-06-10 23:34:59 +0200

    remove deprecated message

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,7 @@
 const dbConfig = require("../config/db.config.js");
 
 const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
 mongoose.Promise = global.Promise;
 
 const db = {};

cb01838f7facb5078292ef22090aa913248987bb
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-06-10 23:47:33 +0200

    add blocked field

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,45 +1,50 @@
 module.exports = mongoose => {
-    const schema = mongoose.Schema(
-        {
-          email: {
-                type: String,
-                required: true,
-                unique: true
-            },
-            name: {
-                type: String,
-                required: true,
-            },
-            lastName: {
-                type: String,
-                required: true,
-            },
-            password: {
-                type: String,
-                required: true
-            },
-            role: {
-                type: String,
-                default: 'ADMIN',
-                required: true,
-                enum: ['USER', 'ADMIN', 'ROOT'],
-            },
-            isVerified: {
-                type: Boolean,
-                default: false,
-                required: true,
-            },
-        },
-        {timestamps: true}
-    );
+  const schema = mongoose.Schema(
+    {
+      email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      role: {
+        type: String,
+        default: "ADMIN",
+        required: true,
+        enum: ["USER", "ADMIN", "ROOT"],
+      },
+      isVerified: {
+        type: Boolean,
+        default: false,
+        required: true,
+      },
+      isBlocked: {
+        type: Boolean,
+        default: false,
+        required: true,
+      },
+    },
+    { timestamps: true }
+  );
 
-    schema.method("toJSON", function () {
-        const {__v, _id, ...object} = this.toObject();
-        object.id = _id;
-        delete object.password;
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    delete object.password;
 
-        return object;
-    });
+    return object;
+  });
 
-    return mongoose.model("user", schema);
+  return mongoose.model("user", schema);
 };
